d5cb3a9483c21fe596b935962ff797bddb07438c
deji12 <adesolaayodeji53@gmail.com>
2022-06-11 17:44:43 +0800

    switched from sqlite to postgress

--- movie/settings.py
+++ movie/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6h49jqsaeh2q0',
+        'USER': 'pxxluardqylbko',
+        'PASSWORD': '9b679ae75f133da838af71a8253711f41725d47c21634b746d408b896acbf6dd',
+        'HOST': 'ec2-34-230-153-41.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
